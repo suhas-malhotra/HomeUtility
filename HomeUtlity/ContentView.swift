742874832743a4e32ad2b12cd039cb1a8c29fb78
Suhas Malhotra <xyzcyz00@gmail.com>
2021-06-16 19:43:10 +0530

    Initial Commit

---
+++ HomeUtlity/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HomeUtlity
+//
+//  Created by Suhas Malhotra on 16/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HomeUtlity/HomeUtlityApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeUtlityApp.swift
+//  HomeUtlity
+//
+//  Created by Suhas Malhotra on 16/06/21.
+//
+
+import SwiftUI
+
+@main
+struct HomeUtlityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
